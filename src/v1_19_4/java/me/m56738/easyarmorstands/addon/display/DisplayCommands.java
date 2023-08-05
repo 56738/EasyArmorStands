bac4c0e1a174d42b3c78a8165a6cadeccae02342
56738 <git@56738.me>
2023-08-05 14:56:36 +0200

    Move display addon commands into another file

---
+++ src/v1_19_4/java/me/m56738/easyarmorstands/addon/display/DisplayCommands.java
@@ -0,0 +1,258 @@
+package me.m56738.easyarmorstands.addon.display;
+
+import cloud.commandframework.annotations.Argument;
+import cloud.commandframework.annotations.CommandMethod;
+import cloud.commandframework.annotations.CommandPermission;
+import cloud.commandframework.annotations.specifier.Range;
+import me.m56738.easyarmorstands.EasyArmorStands;
+import me.m56738.easyarmorstands.bone.v1_19_4.DisplayBone;
+import me.m56738.easyarmorstands.bone.v1_19_4.DisplayTranslationBone;
+import me.m56738.easyarmorstands.command.annotation.RequireEntity;
+import me.m56738.easyarmorstands.command.annotation.RequireSession;
+import me.m56738.easyarmorstands.command.sender.EasPlayer;
+import me.m56738.easyarmorstands.history.action.Action;
+import me.m56738.easyarmorstands.history.action.EntityDestroyAction;
+import me.m56738.easyarmorstands.history.action.EntitySpawnAction;
+import me.m56738.easyarmorstands.node.v1_19_4.DisplayMenuNode;
+import me.m56738.easyarmorstands.session.EntitySpawner;
+import me.m56738.easyarmorstands.session.Session;
+import me.m56738.easyarmorstands.util.ArmorStandPart;
+import me.m56738.easyarmorstands.util.Util;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.Display;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemDisplay;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.util.EulerAngle;
+import org.joml.Math;
+import org.joml.Matrix4d;
+import org.joml.Matrix4dc;
+import org.joml.Quaternionf;
+import org.joml.Vector3d;
+import org.joml.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CommandMethod("eas")
+public class DisplayCommands {
+    private final DisplayAddon addon;
+
+    public DisplayCommands(DisplayAddon addon) {
+        this.addon = addon;
+    }
+
+    @CommandMethod("block <value>")
+    @CommandPermission("easyarmorstands.property.display.block")
+    @RequireSession
+    @RequireEntity(BlockDisplay.class)
+    public void setBlock(Audience sender, Session session, BlockDisplay entity, @Argument("value") BlockData value) {
+        if (session.tryChange(entity, addon.getBlockDisplayBlockProperty(), value)) {
+            sender.sendMessage(Component.text("Changed block to ", NamedTextColor.GREEN)
+                    .append(addon.getBlockDisplayBlockProperty().getValueName(value)));
+        } else {
+            sender.sendMessage(Component.text("Unable to change block", NamedTextColor.RED));
+        }
+    }
+
+    @CommandMethod("brightness block <value>")
+    @CommandPermission("easyarmorstands.property.display.brightness")
+    @RequireSession
+    @RequireEntity(Display.class)
+    public void setBlockBrightness(Audience sender, Session session, Display entity, @Argument("value") @Range(min = "0", max = "15") int value) {
+        int skyLight = addon.getDisplayBrightnessProperty().getValue(entity).map(Display.Brightness::getSkyLight)
+                .orElseGet(() -> (int) entity.getLocation().getBlock().getLightFromSky());
+        Display.Brightness brightness = new Display.Brightness(value, skyLight);
+        if (session.tryChange(entity, addon.getDisplayBrightnessProperty(), Optional.of(brightness))) {
+            sender.sendMessage(Component.text("Changed block brightness to ", NamedTextColor.GREEN)
+                    .append(Component.text(value)));
+        } else {
+            sender.sendMessage(Component.text("Unable to change brightness", NamedTextColor.RED));
+        }
+    }
+
+    @CommandMethod("brightness sky <value>")
+    @CommandPermission("easyarmorstands.property.display.brightness")
+    @RequireSession
+    @RequireEntity(Display.class)
+    public void setSkyBrightness(Audience sender, Session session, Display entity, @Argument("value") @Range(min = "0", max = "15") int value) {
+        int blockLight = addon.getDisplayBrightnessProperty().getValue(entity).map(Display.Brightness::getBlockLight)
+                .orElseGet(() -> (int) entity.getLocation().getBlock().getLightFromBlocks());
+        Display.Brightness brightness = new Display.Brightness(blockLight, value);
+        if (session.tryChange(entity, addon.getDisplayBrightnessProperty(), Optional.of(brightness))) {
+            sender.sendMessage(Component.text("Changed sky brightness to ", NamedTextColor.GREEN)
+                    .append(Component.text(value)));
+        } else {
+            sender.sendMessage(Component.text("Unable to change brightness", NamedTextColor.RED));
+        }
+    }
+
+    @CommandMethod("brightness here")
+    @CommandPermission("easyarmorstands.property.display.brightness")
+    @RequireSession
+    @RequireEntity(Display.class)
+    public void setLocalBrightness(EasPlayer sender, Session session, Display entity) {
+        Block block = sender.get().getLocation().getBlock();
+        Display.Brightness brightness = new Display.Brightness(block.getLightFromBlocks(), block.getLightFromSky());
+        if (session.tryChange(entity, addon.getDisplayBrightnessProperty(), Optional.of(brightness))) {
+            sender.sendMessage(Component.text("Changed entity brightness to the light level at your location", NamedTextColor.GREEN));
+            sender.sendMessage(Component.text("Block light: ", NamedTextColor.GRAY).append(Component.text(brightness.getBlockLight())));
+            sender.sendMessage(Component.text("Sky light: ", NamedTextColor.GRAY).append(Component.text(brightness.getSkyLight())));
+        } else {
+            sender.sendMessage(Component.text("Unable to change brightness", NamedTextColor.RED));
+        }
+    }
+
+    @CommandMethod("brightness reset")
+    @CommandPermission("easyarmorstands.property.display.brightness")
+    @RequireSession
+    @RequireEntity(Display.class)
+    public void setDefaultBrightness(Audience sender, Session session, Display entity) {
+        if (session.tryChange(entity, addon.getDisplayBrightnessProperty(), Optional.empty())) {
+            sender.sendMessage(Component.text("Removed custom brightness settings", NamedTextColor.GREEN));
+        } else {
+            sender.sendMessage(Component.text("Unable to change brightness", NamedTextColor.RED));
+        }
+    }
+
+    @CommandMethod("translation")
+    @CommandPermission("easyarmorstands.property.display.translation")
+    @RequireSession
+    @RequireEntity(Display.class)
+    public void editTranslation(Session session, Display entity) {
+        DisplayTranslationBone translationBone = new DisplayTranslationBone(session, entity, addon);
+        DisplayMenuNode node = new DisplayMenuNode(session, Component.text("Translation", NamedTextColor.GOLD), entity);
+        node.addPositionButtons(session, translationBone, 3);
+        node.addCarryButton(session, translationBone);
+        session.pushNode(node);
+    }
+
+    @CommandMethod("scale <scale>")
+    @CommandPermission("easyarmorstands.property.display.scale")
+    @RequireSession
+    @RequireEntity(Display.class)
+    public void editScale(Audience sender, Session session, Display entity, @Argument("scale") float scale) {
+        Vector3f value = new Vector3f(scale);
+        if (session.tryChange(entity, addon.getDisplayScaleProperty(), value)) {
+            sender.sendMessage(Component.text("Changed scale to ", NamedTextColor.GREEN)
+                    .append(addon.getDisplayScaleProperty().getValueName(value)));
+        } else {
+            sender.sendMessage(Component.text("Unable to change scale", NamedTextColor.RED));
+        }
+    }
+
+    @CommandMethod("shear")
+    @CommandPermission("easyarmorstands.property.display.shearing")
+    @RequireSession
+    @RequireEntity(Display.class)
+    public void editRightRotation(Session session, Display entity) {
+        DisplayBone rotationBone = new DisplayBone(session, entity, addon, addon.getDisplayRightRotationProperty());
+        DisplayMenuNode node = new DisplayMenuNode(session, Component.text("Shearing", NamedTextColor.GOLD), entity);
+        node.addRotationButtons(session, rotationBone, 1, null);
+        session.pushNode(node);
+    }
+
+    @CommandMethod("convert")
+    @CommandPermission("easyarmorstands.convert")
+    @RequireSession
+    @RequireEntity(ArmorStand.class)
+    public void convert(Session session, ArmorStand entity) {
+        EntityEquipment equipment = entity.getEquipment();
+        if (equipment == null) {
+            return;
+        }
+
+        Matrix4d headMatrix = new Matrix4d();
+        Matrix4d rightMatrix = new Matrix4d();
+        Matrix4d leftMatrix = new Matrix4d();
+
+        if (entity.isSmall()) {
+            headMatrix.scale(0.7);
+            rightMatrix.scale(0.5);
+            leftMatrix.scale(0.5);
+        }
+
+        if (isSkull(equipment.getHelmet())) {
+            headMatrix.scale(1.1875);
+            headMatrix.translate(0, 0.5, 0);
+            headMatrix.rotateY(Math.PI);
+        } else {
+            headMatrix.translate(0, 0.25, 0);
+            headMatrix.scale(0.625);
+        }
+
+        rightMatrix.translate(-0.0625, -0.625, 0.125);
+        rightMatrix.rotateX(Math.PI / 2);
+
+        leftMatrix.translate(0.0625, -0.625, 0.125);
+        leftMatrix.rotateX(Math.PI / 2);
+
+        if (Bukkit.getBukkitVersion().equals("1.19.4-R0.1-SNAPSHOT")) {
+            headMatrix.rotateY(Math.PI);
+            rightMatrix.rotateY(Math.PI);
+            leftMatrix.rotateY(Math.PI);
+        }
+
+        List<Action> actions = new ArrayList<>();
+        convert(session, entity, equipment.getHelmet(), ArmorStandPart.HEAD, ItemDisplay.ItemDisplayTransform.HEAD, headMatrix, actions);
+        convert(session, entity, equipment.getItemInMainHand(), ArmorStandPart.RIGHT_ARM, ItemDisplay.ItemDisplayTransform.THIRDPERSON_RIGHTHAND, rightMatrix, actions);
+        convert(session, entity, equipment.getItemInOffHand(), ArmorStandPart.LEFT_ARM, ItemDisplay.ItemDisplayTransform.THIRDPERSON_LEFTHAND, leftMatrix, actions);
+        if (actions.isEmpty()) {
+            session.sendMessage(Component.text("Unable to convert", NamedTextColor.RED));
+            return;
+        }
+
+        actions.add(new EntityDestroyAction<>(entity));
+        entity.remove();
+
+        EasyArmorStands.getInstance().getHistory(session.getPlayer()).push(actions);
+    }
+
+    private boolean isSkull(ItemStack item) {
+        return item != null && item.getItemMeta() instanceof SkullMeta;
+    }
+
+    private void convert(Session session, ArmorStand entity, ItemStack item, ArmorStandPart part, ItemDisplay.ItemDisplayTransform itemTransform, Matrix4dc matrix, List<Action> actions) {
+        if (item == null || item.getType().isAir()) {
+            return;
+        }
+
+        Location location = entity.getLocation();
+        Vector3d offset = part.getOffset(entity).rotateY(Util.getEntityYawRotation(location.getYaw()), new Vector3d());
+        location.add(offset.x, offset.y, offset.z);
+
+        EulerAngle angle = part.getPose(entity);
+        Matrix4d transform = new Matrix4d()
+                .rotateY(Util.getEntityYawRotation(location.getYaw()))
+                .rotateZYX(-angle.getZ(), -angle.getY(), angle.getX())
+                .mul(matrix);
+
+        EntitySpawner<ItemDisplay> spawner = EntitySpawner.of(EntityType.ITEM_DISPLAY, e -> {
+            e.setItemStack(item);
+            e.setItemDisplayTransform(itemTransform);
+            e.setTransformation(addon.getMapper().getTransformation(
+                    transform.getTranslation(new Vector3d()).get(new Vector3f()),
+                    transform.getUnnormalizedRotation(new Quaternionf()),
+                    transform.getScale(new Vector3d()).get(new Vector3f()),
+                    new Quaternionf()
+            ));
+        });
+        ItemDisplay display = EntitySpawner.trySpawn(spawner, location, session.getPlayer());
+        if (display == null) {
+            return;
+        }
+
+        actions.add(new EntitySpawnAction<>(location, spawner, display.getUniqueId()));
+    }
+}
