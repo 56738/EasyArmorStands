14708f8d28addef16429df89a3f4c55c6abd5840
56738 <git@56738.me>
2023-08-08 10:09:26 +0200

    Always commit when navigating nodes

--- src/main/java/me/m56738/easyarmorstands/session/Session.java
+++ src/main/java/me/m56738/easyarmorstands/session/Session.java
@@ -94,6 +94,7 @@ public final class Session implements ChangeContext, ForwardingAudience.Single {
         if (!nodeStack.isEmpty()) {
             nodeStack.peek().onExit();
         }
+        commit();
         nodeStack.push(node);
         node.onAdd();
         node.onEnter();
@@ -103,6 +104,7 @@ public final class Session implements ChangeContext, ForwardingAudience.Single {
         Node removed = nodeStack.pop();
         removed.onExit();
         removed.onRemove();
+        commit();
         nodeStack.push(node);
         node.onAdd();
         node.onEnter();
@@ -112,6 +114,7 @@ public final class Session implements ChangeContext, ForwardingAudience.Single {
         Node removed = nodeStack.pop();
         removed.onExit();
         removed.onRemove();
+        commit();
         if (!nodeStack.isEmpty()) {
             nodeStack.peek().onEnter();
         }
@@ -127,6 +130,7 @@ public final class Session implements ChangeContext, ForwardingAudience.Single {
             }
         }
         nodeStack.clear();
+        commit();
         nodeStack.push(rootNode);
         rootNode.onEnter();
     }
