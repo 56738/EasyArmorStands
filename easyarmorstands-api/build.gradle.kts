9e9d5fa3e00f8cee1dbe9ce93040bd1befef411e
56738 <git@56738.me>
2023-09-26 20:06:51 +0200

    Publish relocated API jar to Maven

--- easyarmorstands-api/build.gradle.kts
+++ easyarmorstands-api/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("easyarmorstands.base")
     id("easyarmorstands.publish")
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
@@ -9,4 +10,25 @@ dependencies {
     api(libs.adventure.text.minimessage)
     api(libs.configurate.core)
     api(libs.joml)
+}
+
+tasks {
+    assemble {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        inputs.property("version", version)
+        filesMatching("*.yml") {
+            expand("version" to version)
+        }
+    }
+
+    shadowJar {
+        val prefix = "me.m56738.easyarmorstands.lib"
+        relocate("io.leangen.geantyref", "$prefix.geantyref")
+        relocate("net.kyori", "$prefix.kyori")
+        relocate("org.joml", "$prefix.joml")
+        relocate("org.spongepowered.configurate", "$prefix.configurate")
+    }
 }
