9cea6b918a1716c9fa0d58e6de30c202ba70c250
TWME <65117253+TWME-TW@users.noreply.github.com>
2024-06-14 17:09:47 +0800

    Update and check whether a location can be broken and placed

--- easyarmorstands-region/easyarmorstands-griefdefender/src/main/java/me/m56738/easyarmorstands/griefdefender/GriefDefenderPrivilegeChecker.java
+++ easyarmorstands-region/easyarmorstands-griefdefender/src/main/java/me/m56738/easyarmorstands/griefdefender/GriefDefenderPrivilegeChecker.java
@@ -1,11 +1,14 @@
 package me.m56738.easyarmorstands.griefdefender;
 
 import com.griefdefender.api.Core;
+import com.griefdefender.api.User;
 import com.griefdefender.api.claim.Claim;
 import com.griefdefender.api.claim.TrustTypes;
 import me.m56738.easyarmorstands.region.RegionPrivilegeChecker;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class GriefDefenderPrivilegeChecker implements RegionPrivilegeChecker {
     private final Core core;
@@ -19,10 +22,16 @@ public class GriefDefenderPrivilegeChecker implements RegionPrivilegeChecker {
         if (!core.isEnabled(location.getWorld().getUID())) {
             return true;
         }
+
         Claim claim = core.getClaimAt(location);
-        if (claim == null || claim.isWilderness()) {
+        if (claim == null) {
             return true;
         }
-        return claim.isUserTrusted(player.getUniqueId(), TrustTypes.BUILDER);
+
+        User user = core.getUser(player.getUniqueId());
+        if (user == null) {
+            return true;
+        }
+        return user.canBreak(location) && user.canPlace((new ItemStack(Material.ARMOR_STAND)), location);
     }
 }
