b265895eff4ea552c6f427b98cb97b506cdf7a6b
56738 <git@56738.me>
2025-01-21 16:36:13 +0100

    Restrict entity location

--- easyarmorstands-plugin/src/main/java/me/m56738/easyarmorstands/property/entity/EntityLocationProperty.java
+++ easyarmorstands-plugin/src/main/java/me/m56738/easyarmorstands/property/entity/EntityLocationProperty.java
@@ -1,13 +1,17 @@
 package me.m56738.easyarmorstands.property.entity;
 
+import me.m56738.easyarmorstands.api.property.PendingChange;
 import me.m56738.easyarmorstands.api.property.Property;
 import me.m56738.easyarmorstands.api.property.type.EntityPropertyTypes;
 import me.m56738.easyarmorstands.api.property.type.PropertyType;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class EntityLocationProperty implements Property<Location> {
+    private static final double HEIGHT_LIMIT = 20000000;
+    private static final double COORDINATE_LIMIT = 30000000;
     private final Entity entity;
 
     public EntityLocationProperty(Entity entity) {
@@ -25,11 +29,38 @@ public class EntityLocationProperty implements Property<Location> {
     }
 
     @Override
+    public @Nullable PendingChange prepareChange(@NotNull Location value) {
+        if (!isValid(value)) {
+            return null;
+        }
+
+        return Property.super.prepareChange(value);
+    }
+
+    @Override
     public boolean setValue(@NotNull Location value) {
+        if (!isValid(value)) {
+            return false;
+        }
+
         boolean ok = entity.teleport(value);
         if (ok) {
             entity.setFallDistance(0);
         }
         return ok;
+    }
+
+    private boolean isValid(@NotNull Location location) {
+        return isValid(location.getX(), COORDINATE_LIMIT) &&
+                isValid(location.getY(), HEIGHT_LIMIT) &&
+                isValid(location.getZ(), COORDINATE_LIMIT);
+    }
+
+    private boolean isValid(double value, double limit) {
+        return isValid(value, -limit, limit);
+    }
+
+    private boolean isValid(double value, double min, double max) {
+        return value >= min && value < max;
     }
 }
