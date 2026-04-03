c7c3095dc5f93d958ca0d31adcaba2efabb0011d
56738 <git@56738.me>
2026-04-03 11:22:36 +0200

    Move capabilities into main source set

--- easyarmorstands-plugin/build.gradle.kts
+++ easyarmorstands-plugin/build.gradle.kts
@@ -66,55 +66,6 @@ tasks {
     }
 }
 
-fun registerSourceSet(name: String) {
-    val sourceSet = sourceSets.register(name) {
-        compileClasspath += sourceSets.main.get().output
-        runtimeClasspath += sourceSets.main.get().output
-    }
-
-    configurations.named("${name}CompileOnly") {
-        extendsFrom(configurations.compileOnlyApi.get())
-    }
-
-    configurations.named("${name}Implementation") {
-        extendsFrom(configurations.implementation.get())
-    }
-
-    tasks {
-        shadowJar {
-            from(sourceSet.map { it.output })
-        }
-    }
-}
-
-fun registerVersion(name: String, api: String) {
-    registerSourceSet(name)
-    dependencies {
-        "${name}CompileOnly"(api)
-    }
-}
-
-registerVersion("v1_8", "org.bukkit:bukkit:1.8.8-R0.1-SNAPSHOT")
-registerVersion("v1_9", "org.bukkit:bukkit:1.9-R0.1-SNAPSHOT")
-registerVersion("v1_9_spigot", "org.spigotmc:spigot-api:1.9.4-R0.1-SNAPSHOT")
-registerVersion("v1_10_2", "org.bukkit:bukkit:1.10.2-R0.1-SNAPSHOT")
-registerVersion("v1_11", "org.bukkit:bukkit:1.11-R0.1-SNAPSHOT")
-registerVersion("v1_12", "org.bukkit:bukkit:1.12-R0.1-SNAPSHOT")
-registerVersion("v1_12_paper", "com.destroystokyo.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
-registerVersion("v1_13", "org.bukkit:bukkit:1.13-R0.1-SNAPSHOT")
-registerVersion("v1_14", "org.bukkit:bukkit:1.14-R0.1-SNAPSHOT")
-registerVersion("v1_15_2", "org.bukkit:bukkit:1.15.2-R0.1-SNAPSHOT")
-registerVersion("v1_16", "org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
-registerVersion("v1_16_paper", "com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-registerVersion("v1_18", "org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
-registerVersion("v1_18_paper", "io.papermc.paper:paper-api:1.18-R0.1-SNAPSHOT")
-registerVersion("v1_19_4", "org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
-registerVersion("v1_20_2", "org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
-registerVersion("v1_20_6", "org.spigotmc:spigot-api:1.20.6-R0.1-SNAPSHOT")
-registerVersion("v1_20_6_paper", "io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
-registerVersion("v1_21_10_spigot", "org.spigotmc:spigot-api:1.21.10-R0.1-SNAPSHOT")
-registerVersion("v1_21_10_paper", "io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
-
 val supportedGameVersions = listOf(
     "1.21.11",
 )
