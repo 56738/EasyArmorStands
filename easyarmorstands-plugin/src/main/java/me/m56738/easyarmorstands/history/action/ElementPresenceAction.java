b135d3865af6f01f90dc2e40feef432c51123cf3
56738 <git@56738.me>
2026-04-12 15:39:55 +0200

    Use entity snapshot to undo destroying entities

--- easyarmorstands-plugin/src/main/java/me/m56738/easyarmorstands/history/action/ElementPresenceAction.java
+++ easyarmorstands-plugin/src/main/java/me/m56738/easyarmorstands/history/action/ElementPresenceAction.java
@@ -5,21 +5,39 @@ import me.m56738.easyarmorstands.api.element.DestroyableElement;
 import me.m56738.easyarmorstands.api.element.Element;
 import me.m56738.easyarmorstands.api.element.ElementReference;
 import me.m56738.easyarmorstands.api.element.ElementType;
+import me.m56738.easyarmorstands.api.element.EntityElement;
 import me.m56738.easyarmorstands.api.element.EntityElementReference;
+import me.m56738.easyarmorstands.api.element.EntityElementType;
 import me.m56738.easyarmorstands.api.property.PropertyContainer;
 import me.m56738.easyarmorstands.api.property.PropertyMap;
+import me.m56738.easyarmorstands.api.property.type.EntityPropertyTypes;
 import me.m56738.easyarmorstands.context.ChangeContext;
+import me.m56738.easyarmorstands.permission.Permissions;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntitySnapshot;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.Nullable;
 
 import java.util.UUID;
 
+@SuppressWarnings("UnstableApiUsage")
 abstract class ElementPresenceAction implements Action {
     private final PropertyContainer properties;
+    private final @Nullable EntitySnapshot snapshot;
     private ElementReference reference;
 
     public ElementPresenceAction(@NotNull Element element) {
         this.properties = new PropertyMap(element.getProperties());
+        this.snapshot = getSnapshot(element);
         this.reference = element.getReference();
+    }
+
+    private static @Nullable EntitySnapshot getSnapshot(Element element) {
+        if (element instanceof EntityElement<?> entityElement) {
+            return entityElement.getEntity().createSnapshot();
+        } else {
+            return null;
+        }
     }
 
     protected boolean create(ChangeContext context) {
@@ -28,7 +46,13 @@ abstract class ElementPresenceAction implements Action {
             return false;
         }
 
-        Element element = type.createElement(properties);
+        Element element = null;
+        if (type instanceof EntityElementType<?> entityElementType && context.permissions().test(Permissions.COPY_ENTITY)) {
+            element = createEntity(entityElementType);
+        }
+        if (element == null) {
+            element = type.createElement(properties);
+        }
         if (element == null) {
             return false;
         }
@@ -49,17 +73,24 @@ abstract class ElementPresenceAction implements Action {
         }
 
         Element element = reference.getElement();
-        if (!(element instanceof DestroyableElement)) {
+        if (!(element instanceof DestroyableElement destroyableElement)) {
             return false;
         }
 
-        DestroyableElement destroyableElement = (DestroyableElement) element;
         if (!context.canDestroyElement(destroyableElement)) {
             return false;
         }
 
         destroyableElement.destroy();
         return true;
+    }
+
+    private <E extends Entity> @Nullable Element createEntity(EntityElementType<E> type) {
+        if (snapshot == null) {
+            return null;
+        }
+        E entity = type.getEntityClass().cast(snapshot.createEntity(properties.get(EntityPropertyTypes.LOCATION).getValue()));
+        return type.getElement(entity);
     }
 
     @Override
