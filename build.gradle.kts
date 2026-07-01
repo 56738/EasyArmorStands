7b5f71bd69df5b5b9f1ca16988b597e3c646280f
56738 <git@56738.me>
2026-07-01 08:47:16 +0200

    Generate paper-plugin.yml using resource-factory

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import io.papermc.hangarpublishplugin.model.Platforms
+import xyz.jpenilla.resourcefactory.paper.PaperPluginYaml
 
 plugins {
     id("easyarmorstands.base")
     alias(libs.plugins.shadow)
     alias(libs.plugins.hangar.publish)
     alias(libs.plugins.minotaur)
+    alias(libs.plugins.resource.factory.paper.convention)
     alias(libs.plugins.run.paper)
 }
 
@@ -38,17 +40,6 @@ dependencies {
 tasks {
     runServer {
         minecraftVersion(libs.versions.minecraft.get())
-    }
-
-    processResources {
-        val props = mapOf(
-            "version" to version,
-            "minecraftVersion" to libs.versions.minecraft.get()
-        )
-        inputs.properties(props)
-        filesMatching("*.yml") {
-            expand(props)
-        }
     }
 
     jar {
@@ -90,6 +81,126 @@ val supportedGameVersions = listOf(
     "26.1.2",
     "26.2",
 )
+
+paperPluginYaml {
+    apiVersion = libs.versions.minecraft
+    main = "me.m56738.easyarmorstands.EasyArmorStandsPlugin"
+    bootstrapper = "me.m56738.easyarmorstands.EasyArmorStandsBootstrap"
+    author = "56738"
+
+    dependencies {
+        server("BentoBox", PaperPluginYaml.Load.BEFORE, false)
+        server("FancyHolograms", PaperPluginYaml.Load.BEFORE, false)
+        server("GriefDefender", PaperPluginYaml.Load.BEFORE, false)
+        server("HeadDatabase", PaperPluginYaml.Load.BEFORE, false)
+        server("HuskClaims", PaperPluginYaml.Load.BEFORE, false)
+        server("Lands", PaperPluginYaml.Load.BEFORE, false)
+        server("PlotSquared", PaperPluginYaml.Load.BEFORE, false)
+        server("Residence", PaperPluginYaml.Load.BEFORE, false)
+        server("Towny", PaperPluginYaml.Load.BEFORE, false)
+        server("Train_Carts", PaperPluginYaml.Load.BEFORE, false)
+        server("ViaVersion", PaperPluginYaml.Load.BEFORE, false)
+        server("WorldGuard", PaperPluginYaml.Load.BEFORE, false)
+    }
+
+    permissions {
+        register("easyarmorstands.survival") {
+            description = "Allow editing armor stands and their basic properties"
+            children("easyarmorstands.align")
+            children("easyarmorstands.clipboard")
+            children("easyarmorstands.edit.armorstand")
+            children("easyarmorstands.give")
+            children("easyarmorstands.property.armorstand.arms")
+            children("easyarmorstands.property.armorstand.baseplate")
+            children("easyarmorstands.property.armorstand.cantick")
+            children("easyarmorstands.property.armorstand.lock")
+            children("easyarmorstands.property.armorstand.pose.body")
+            children("easyarmorstands.property.armorstand.pose.head")
+            children("easyarmorstands.property.armorstand.pose.leftarm")
+            children("easyarmorstands.property.armorstand.pose.leftleg")
+            children("easyarmorstands.property.armorstand.pose.rightarm")
+            children("easyarmorstands.property.armorstand.pose.rightleg")
+            children("easyarmorstands.property.armorstand.size")
+            children("easyarmorstands.property.equipment.hand")
+            children("easyarmorstands.property.equipment.offhand")
+            children("easyarmorstands.property.equipment.feet")
+            children("easyarmorstands.property.equipment.legs")
+            children("easyarmorstands.property.equipment.chest")
+            children("easyarmorstands.property.equipment.head")
+            children("easyarmorstands.property.equipment.body")
+            children("easyarmorstands.property.gravity")
+            children("easyarmorstands.property.location")
+            children("easyarmorstands.property.visible")
+            children("easyarmorstands.select")
+            children("easyarmorstands.snap")
+        }
+
+        register("easyarmorstands.creative") {
+            description = "Allow spawning and editing armor stands, display entities and mannequins"
+            children("easyarmorstands.survival")
+            children("easyarmorstands.clone")
+            children("easyarmorstands.color")
+            children("easyarmorstands.convert")
+            children("easyarmorstands.copy.entity")
+            children("easyarmorstands.destroy.armorstand")
+            children("easyarmorstands.destroy.blockdisplay")
+            children("easyarmorstands.destroy.interaction")
+            children("easyarmorstands.destroy.itemdisplay")
+            children("easyarmorstands.destroy.mannequin")
+            children("easyarmorstands.destroy.textdisplay")
+            children("easyarmorstands.edit.blockdisplay")
+            children("easyarmorstands.edit.interaction")
+            children("easyarmorstands.edit.itemdisplay")
+            children("easyarmorstands.edit.mannequin")
+            children("easyarmorstands.edit.textdisplay")
+            children("easyarmorstands.position")
+            children("easyarmorstands.property.ai")
+            children("easyarmorstands.property.armorstand.marker")
+            children("easyarmorstands.property.display.billboard")
+            children("easyarmorstands.property.display.block")
+            children("easyarmorstands.property.display.brightness")
+            children("easyarmorstands.property.display.glow.color")
+            children("easyarmorstands.property.display.item")
+            children("easyarmorstands.property.display.item.transform")
+            children("easyarmorstands.property.display.rotation")
+            children("easyarmorstands.property.display.scale")
+            children("easyarmorstands.property.display.shearing")
+            children("easyarmorstands.property.display.size")
+            children("easyarmorstands.property.display.text")
+            children("easyarmorstands.property.display.text.alignment")
+            children("easyarmorstands.property.display.text.background")
+            children("easyarmorstands.property.display.text.linewidth")
+            children("easyarmorstands.property.display.text.seethrough")
+            children("easyarmorstands.property.display.text.shadow")
+            children("easyarmorstands.property.display.translation")
+            children("easyarmorstands.property.glow")
+            children("easyarmorstands.property.interaction.responsive")
+            children("easyarmorstands.property.invulnerable")
+            children("easyarmorstands.property.mannequin.description")
+            children("easyarmorstands.property.mannequin.immovable")
+            children("easyarmorstands.property.mannequin.mainhand")
+            children("easyarmorstands.property.mannequin.part.visible.cape")
+            children("easyarmorstands.property.mannequin.part.visible.hat")
+            children("easyarmorstands.property.mannequin.part.visible.jacket")
+            children("easyarmorstands.property.mannequin.part.visible.pants.left")
+            children("easyarmorstands.property.mannequin.part.visible.pants.right")
+            children("easyarmorstands.property.mannequin.part.visible.sleeve.left")
+            children("easyarmorstands.property.mannequin.part.visible.sleeve.right")
+            children("easyarmorstands.property.mannequin.profile")
+            children("easyarmorstands.property.name")
+            children("easyarmorstands.property.name.visible")
+            children("easyarmorstands.property.silent")
+            children("easyarmorstands.redo")
+            children("easyarmorstands.spawn.armorstand")
+            children("easyarmorstands.spawn.blockdisplay")
+            children("easyarmorstands.spawn.interaction")
+            children("easyarmorstands.spawn.itemdisplay")
+            children("easyarmorstands.spawn.mannequin")
+            children("easyarmorstands.spawn.textdisplay")
+            children("easyarmorstands.undo")
+        }
+    }
+}
 
 modrinth {
     projectId = "easyarmorstands"
